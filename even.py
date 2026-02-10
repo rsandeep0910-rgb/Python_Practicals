3365e5678ec13adce118536aea390a88353007c1
sandeep <rsandeep0910@gmail.com>
2026-02-10 22:55:21 +0530

    Adding even numbers

---
+++ even.py
@@ -0,0 +1,10 @@
+print("Even numbers from 1 to 10")
+for number in range(1, 21):
+    if number % 2==0:
+        print(number)
+
+
+print("Odd numbers from 1 to 10")
+for number in range(1, 21):
+    if number % 2 != 0:
+        print(number)
